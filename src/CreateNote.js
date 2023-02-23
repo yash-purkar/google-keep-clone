4c13725bb47c830204e11d5eff0ebb9c8c6da0a6
yashpurkar <yashpurkar7079@gmail.com>
2023-02-23 14:21:33 +0530

    done

--- src/CreateNote.js
+++ src/CreateNote.js
@@ -40,31 +40,31 @@ const CreateNote = () => {
   return (
     <>
       <div className='flex flex-col items-center justify-center'>
-        <input type="text" name="" value={title} onChange={(e) => setTitle(e.target.value)} placeholder="Title.." className="  w-56 border h-10  border-yellow-500 rounded-xl px-3 tracking-wider placeholder-gray-500" maxLength={15} />
+        <input type="text" name="" value={title} onChange={(e) => setTitle(e.target.value)} placeholder="Title.." className=" w-64 border h-10  border-yellow-500 rounded-xl px-3 tracking-wider placeholder-gray-500 text-gray-900 focus:outline-yellow-600 sm:w-80 md:w-96" maxLength={15} />
 
-        <textarea name="" id="" cols="30" rows="5" value={content} onChange={(e) => setContent(e.target.value)} placeholder="Write a note..." className=" w-56 p-4 border-2 border-yellow-500 border-t-0 rounded-lg placeholder-gray-400 tracking-wider mb-6 resize-none" maxLength={28} ></textarea>
+        <textarea name="" id="" cols="30" rows="5" value={content} onChange={(e) => setContent(e.target.value)} placeholder="Write a note..." className=" w-64 p-4 border-2 border-yellow-500 border-t-0 rounded-lg placeholder-gray-400 tracking-wider mb-6 text-gray-900 focus:outline-yellow-600 resize-none sm:w-80 md:w-96" maxLength={28} ></textarea>
 
-        <button onClick={handleClick} className="border-1 border-black bg-white  w-8 mb-3"><img src={add} alt="add" /></button>
+        <button onClick={handleClick} className="border-1 border-black bg-white rounded-lg  w-8 mb-3 sm:w-10 md:w-11"><img src={add} alt="add" /></button>
 
-        <p className='h-3  mb-5 text-red-500 tracking-wider'>{errMsg}</p>
+        <p className='h-3 font-bold mb-5 text-red-500 tracking-wider'>{errMsg}</p>
       </div>
 
 
-      <hr className='border- border-yellow-300' />
+      <hr className='border-yellow-300 sm:mx-20  md:mx-52 ' />
 
 
       {
         data.map((elem) => {
           return (
             <>
-              <div key={elem.id} className="flex justify-center flex-col items-center m-auto border border-yellow-500 w-56  overflow-scroll mt-10 bg-white px-3 rounded-md h-28">
+              <div key={elem.id} className="flex justify-center flex-col items-center  m-auto border border-yellow-500 w-56 mt-10 bg-white px-3 rounded-md h-28 sm:w-72 ">
 
                 <h2 className='bg-white w-full text-center px-5 py-1 tracking-wider border-b border-gray-400 font-extrabold '>{elem.title}</h2>
 
-                <p className='bg-white w-full text-center px-5 pt-2  py-2 tracking-wider'>{elem.content}</p>
+                <p className='bg-white w-full text-center px-5 pt-2  py-2 tracking-wider font-serief'>{elem.content}</p>
 
                 <button onClick={() => deleteNote(elem.id)} className=" 
-         px-3 relative w-11"><img src={remove} alt="remove" className='bg-white border rounded-md border-red-500' /></button>
+         px-3 relative w-11 sm:w-12"><img src={remove} alt="remove" className='bg-white border rounded-md border-red-500' /></button>
 
               </div>
             </>

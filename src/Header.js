42e8fc4514469d108e2892f00f89e6805e9b1729
yashpurkar <yashpurkar7079@gmail.com>
2023-02-23 18:34:28 +0530

    style: updated style

--- src/Header.js
+++ src/Header.js
@@ -4,7 +4,7 @@ import logo from './Images/logo.svg'
 const Header = () => {
   return (
     <div className='sticky top-0 flex space-x-2  justify-center bg-yellow-400 mb-10 sm:py-3'>
-      <img src={logo} alt="" className='border-2 border-yellow-200 rounded-xl mr-2' />
+      <img src={logo} alt="" className='mr-2 shadow-2xl' />
       <h1 className='self-center text-white text-lg font-bold tracking-wider selection:sm:text-2xl md:text-3xl'>Keep Notes</h1>
     </div>
   )
